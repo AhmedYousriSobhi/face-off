72ce820bc1c0a619cbbae32a03e55630be6eb4a0
AhmedYousri <ahmedyousrisobhi@gmail.com>
2023-09-10 22:32:29 +0300

    final steps

--- face_match/face_match.py
+++ face_match/face_match.py
@@ -3,11 +3,29 @@
 import recognition
 
 
-label_directory = '../data/label'
+label_path = '../data/label'
 
 # Defining models
 face_recognition_model = 'ArcFace'
 face_detector_model = 'retinaface'
 
+if __name__ == '__main__':
+
+    if task == 'single':
+        _ = recognition.single_image_recognition(
+                img_path=img_path,
+                img_name=img_name,
+                label_path=label_path,
+                model_name=face_detector_model,
+                detector_backend=face_detector_model
+            )
+    
+    elif task == 'multi':
+        _ = recognition.multi_image_recognition(
+                img_path=img_path,
+                label_path=label_path,
+                model_name=face_detector_model,
+                detector_backend=face_detector_model
+            )
 
 
